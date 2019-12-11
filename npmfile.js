88269167c64f98cf926d848f89f4f0f17d8fae86
James Taylor II <james@jetsquared.com>
2019-12-10 21:23:21 -0500

    domain name changes

--- npmfile.js
+++ npmfile.js
@@ -1,3 +1,3 @@
 exports.printMsg = function() {
-    console.log("Visit http://photography.jetsquared.com for a treat!");
+    console.log("Visit https://photography.jetsquared.com for a treat!");
 };
